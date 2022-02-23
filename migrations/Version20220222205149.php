83232e2316c7c025df08a2c3fc727cf4f11175ee
keshiamukenge <mukenge.keshia@gmail.com>
2022-02-23 17:00:52 +0100

    bug

---
+++ migrations/Version20220222205149.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220222205149 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE admin ADD firstname VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE "user" DROP password');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE admin DROP firstname');
+        $this->addSql('ALTER TABLE "user" ADD password VARCHAR(255) NOT NULL');
+    }
+}
