a1e2222c545065fa0c254f6f3dcbf599f1e85451
keshiamukenge <mukenge.keshia@gmail.com>
2022-02-23 18:36:08 +0100

    feat: init account page

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomepageController extends AbstractController
+class AccountController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    #[Route('/account', name: 'account')]
     public function index(): Response
     {
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
         ]);
     }
 }
